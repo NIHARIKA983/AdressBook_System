964b38d27d6d225895e6758ee79edae0a70b89ec
NIHARIKA983 <niharikagowda1996@gmail.com>
2021-08-01 16:21:25 +0530

    Added:Ability to Search Person in a City or State across the multiple AddressBook

--- src/main/java/com/bridgelabz/AddressBookMain.java
+++ src/main/java/com/bridgelabz/AddressBookMain.java
@@ -7,7 +7,7 @@ public class AddressBookMain {
     static HashMap<String,AddressBook>addressbooks=new HashMap<String,AddressBook>();
     public static void main(String[] args) {
         System.out.println("Welcome to Address Book Program !!!");
-
+        @SuppressWarnings("resource")
         Scanner userInput=new Scanner(System.in);
         AddressBook addressbook=new AddressBook();
         int choice;
@@ -16,20 +16,20 @@ public class AddressBookMain {
         do {
             System.out.println("\n\n*******************OPTIONS********************\n");
             System.out.println("1]Add Contact\n2]Edit Contact\n3]Delete Contact\n4]Go To Address Book\n5]Display Current AddressBook\n"
-                    + "6]Display All AddressBooks\n7]Exit");
+                    + "6]Display All AddressBooks\n7]Search Person By City Or State In All Address Books\n8]Exit");
             choice=userInput.nextInt();
             userInput.nextLine();
             switch(choice) {
-                case 1:addressbook.addPerson();
+                case 1: addressbook.addPerson();
                     addressbook.display();
                     break;
-                case 2:addressbook.editPerson();
+                case 2: addressbook.editPerson();
                     addressbook.display();
                     break;
-                case 3:addressbook.deletePerson();
+                case 3: addressbook.deletePerson();
                     addressbook.display();
                     break;
-                case 4:System.out.println("Enter name of Address Book:");
+                case 4: System.out.println("Enter name of Address Book:");
                     addressbookName=userInput.nextLine();
                     if(isKeyExists(addressbookName)) {
                         System.out.println("This addressbook already exists!");
@@ -44,20 +44,30 @@ public class AddressBookMain {
                         System.out.println("Welcome to '"+addressbookName+"' addressbook");
                     }
                     break;
-                case 5:addressbook.display();
+                case 5: addressbook.display();
                     break;
-                case 6:System.out.println("*******All Address Book Details*******\n");
+                case 6: System.out.println("*******All Address Book Details*******\n");
                     display();
                     break;
-                case 7:	System.exit(0);
+                case 7: System.out.println("Enter (C) to search by City\nEnter (S) to search by State\n");
+                    String input=userInput.nextLine();
+                    if(input.equalsIgnoreCase("C"))
+                        searchPersonByCity();
+                    else if(input.equalsIgnoreCase("S"))
+                        searchPersonByState();
+                    else
+                        System.out.println("Invalid Input!");
+                    break;
+                case 8:	System.exit(0);
             }
-        }while(choice!=7);
+        }while(choice!=8);
     }
+
     public static boolean isKeyExists(String name) {
         return addressbooks.containsKey(name);
     }
 
-
+    @SuppressWarnings("rawtypes")
     public static void display() {
         Iterator<Entry<String, AddressBook>> trav=addressbooks.entrySet().iterator();
         while(trav.hasNext()) {
@@ -65,5 +75,41 @@ public class AddressBookMain {
             AddressBook adBook=(AddressBook)record.getValue();
             System.out.println("\""+record.getKey()+"\""+" "+adBook);
         }
+    }
+
+    public static void searchPersonByCity() {
+        Scanner userInput=new Scanner(System.in);
+        System.out.print("Enter city name :");
+        int flag=0;
+        String cityName=userInput.nextLine();
+        for(Map.Entry record : addressbooks.entrySet()){
+            AddressBook addressbook=(AddressBook) record.getValue();
+            for(Person person : addressbook.contacts) {
+                if(person.city.equalsIgnoreCase(cityName)) {
+                    flag=1;
+                    System.out.println(person);
+                }
+            }
+        }
+        if(flag==0)
+            System.out.println("This City does not exists!");
+    }
+
+    public static void searchPersonByState() {
+        Scanner userInput=new Scanner(System.in);
+        System.out.print("Enter State name :");
+        String stateName=userInput.nextLine();
+        int flag=0;
+        for(Map.Entry record : addressbooks.entrySet()){
+            AddressBook addressbook=(AddressBook) record.getValue();
+            for(Person person : addressbook.contacts) {
+                if(person.state.equalsIgnoreCase(stateName)) {
+                    flag=1;
+                    System.out.println(person);
+                }
+            }
+        }
+        if(flag==0)
+            System.out.println("This State does not exists!");
     }
 }
