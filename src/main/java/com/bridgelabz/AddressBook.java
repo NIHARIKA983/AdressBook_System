c18dc3a690cf14841726d198ee78cb4e57ca7a73
NIHARIKA983 <niharikagowda1996@gmail.com>
2021-08-21 21:01:50 +0530

    Added:Updating Query

--- src/main/java/com/bridgelabz/AddressBook.java
+++ src/main/java/com/bridgelabz/AddressBook.java
@@ -1,5 +1,6 @@
 package com.bridgelabz;
 
+import java.sql.SQLException;
 import java.util.List;
 
 public class AddressBook {
@@ -20,9 +21,21 @@ public class AddressBook {
         this.addressBookList = addressBookList;
     }
 
-    public List<AddressBookData> readAddressBookData(IOService ioService) {
+    public List<AddressBookData> readAddressBookData(IOService ioService) throws SQLException {
         if (ioService.equals(IOService.DB_IO))
             return this.addressBookList = addressBookDBService.getAddressBookDataUsingDB();
         return null;
+    }
+    public void updateContact(String address, String name) {
+        int result = addressBookDBService.updateContactDetails(name, address);
+    }
+
+    public boolean checkAddressBookInSyncWithDB(String name) {
+        List<AddressBookData> addressBookDataList = addressBookDBService.getEmployeePayrollData(name);
+        return addressBookDataList.get(0).equals(getEmployeePayrollData(name));
+    }
+
+    private AddressBookData getEmployeePayrollData(String name) {
+        return this.addressBookList.stream().filter(employeePayrollDataItem -> employeePayrollDataItem.firstName.equals(name)).findFirst().orElse(null);
     }
 }
--- src/test/java/com/bridgelabz/AddressBookTest.java
+++ src/test/java/com/bridgelabz/AddressBookTest.java
@@ -3,16 +3,24 @@ package com.bridgelabz;
 import java.util.List;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.Assertions;
-
+import java.sql.SQLException;
 public class AddressBookTest {
     AddressBook addressBook;
     List<AddressBookData> addressBookDataList;
 
     @Test
-    public void givenThreeContactsInDB_WhenRetrieved_ShouldMatchEmployeeCount() {
+    public void givenThreeContactsInDB_WhenRetrieved_ShouldMatchEmployeeCount() throws SQLException {
         addressBook = new AddressBook();
         // addressBookDataList = addressBook.readAddressBookData(AddressBook.IOService.DB_IO);
         //Assertions.assertEquals(3, addressBookDataList.size());
-        Assertions.assertEquals(4,  addressBook.readAddressBookData(AddressBook.IOService.DB_IO).size());
+        Assertions.assertEquals(1, addressBook.readAddressBookData(AddressBook.IOService.DB_IO).size());
+    }
+    @Test
+    public void givenContactDataInDB_whenUpdated_ShouldSyncWithDB() throws SQLException {
+        addressBook = new AddressBook();
+        addressBookDataList = addressBook.readAddressBookData(AddressBook.IOService.DB_IO);
+        addressBook.updateContact("Karnataka", "Niharika");
+        boolean result = addressBook.checkAddressBookInSyncWithDB("Niharika");
+        Assertions.assertTrue(result);
     }
 }
